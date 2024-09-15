1dffc4ffa7ce9da0759427db2ef3ca0d0f3296d3
Ayush <ayushnagar633@gmail.com>
2024-09-16 02:16:42 +0530

    Initial commit for Fruit.ai project

---
+++ src/components/Chat.js
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./Chat.css";
+
+const Chat = () => {
+  const navi = useNavigate();
+
+  const handleClick = (path) => {
+    navi(path); // This will navigate to the given path
+  };
+  return (
+    <div className="chat">
+      <div
+        className="n"
+        onClick={() => handleClick("/chatbot")}
+        style={{ cursor: "pointer" }}
+      >
+        click here
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
